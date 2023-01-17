7375c8cef086f9b8b39691d89d4b52f6f620281a
AlexMakhony <makhovskiy.sasha@gmail.com>
2023-01-17 16:40:53 +0200

    HW 7 Done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,7 +9,7 @@ import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector(`.gallery`);
 
-// 2. Пишемо розмітку перебравши массив galleryItems через map
+// 2. Пишемо розмітку перебравши массив galleryItems через map та вертаючи в стрічку все що нам треба в HTML додати
 
 const markUp = createMarkUp(galleryItems);
 function createMarkUp () {
@@ -34,17 +34,28 @@ gallery.insertAdjacentHTML("beforeend", markUp);
 // 3. Далі працюємо з кліком на картинку
 
 const imgClick = (event) => {
-    // 3.1 Обнуляємо дію браузеру
+
+    // 3.1 Обнуляємо дію браузеру по відкриттю
+
     event.preventDefault();
-    // 3.2 Робимо делегування подій
+
+    // 3.2 Блокуємо клік де попало :)
+
     if (event.target === event.currentTarget) return;
+
+    // 3.3 Робимо делегування подій
+
     const source = event.target.dataset.source;
-    // 3.3 Запускаємо лайтбокс з влаштованної бібліотеки
+
+    // 3.4 Запускаємо лайтбокс з влаштованної бібліотеки
+
     const instance = basicLightbox.create(`
     <img src="${source}" width="800" height="600">
     `);
     instance.show();
-    // 3.4 ESCape
+
+    // 3.5 Знаходимо манул як зробити клоуз по ESC
+
         if (instance.visible()) {
             window.addEventListener("keydown", onPressKeyESC);
         }
@@ -54,7 +65,9 @@ const imgClick = (event) => {
                 window.removeEventListener("keydown", onPressKeyESC);
                 }
             }
-
     }
-    // 3.5 вішаємо слухача
+    // 3.6 вішаємо слухача
+
     gallery.addEventListener(`click`, imgClick);
+
+    // Happy End :)
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// 1. Знаходимо нашу UL-ку
+
+const gallery = document.querySelector(`.gallery`);
+
+// 2. Пишемо розмітку перебравши массив galleryItems через map
+
+const markUp = createMarkUp(galleryItems);
+function createMarkUp () {
+    return galleryItems
+    .map(({preview, original, description}) => {
+
+// 3.  Пепеписуємо наповнення, від себе додав li, бо якось ul без li ріже очі :)
+
+        return `
+        <li>
+        <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+        </a>
+        </li>
+      `
+    })
+    .join("");
+}
+
+// 4. Так само вставляємо наш маркап перед
+
+gallery.insertAdjacentHTML("beforeend", markUp);
+
+
+// 5. Далі юзаємо мануал суто з документації
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    // 5.1 Опис
+    captionsData: "alt",
+    // 5.2 Ділей
+    captionDelay: 250,
+});
